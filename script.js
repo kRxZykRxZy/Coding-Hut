2acd0586950e54ada70cdc0efa0ce9859e81b8ca
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-02-27 18:54:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Functions for redirecting to different pages
 function goToPage1() {
-  window.location.href = '/Coding-Hut/index.html';
+  window.location.href = 'Coding-Hut/index.html';
 }
 
 function goToPage2() {
-  window.location.href = '/Coding-Hut/ordering.html';
+  window.location.href = 'Coding-Hut/ordering.html';
 }
 
 function goToPage3() {
-  window.location.href = '/Coding-Hut/rules.html';
+  window.location.href = 'Coding-Hut/rules.html';
 }
 
 function goToPage4() {
-  window.location.href = '/Coding-Hut/login.html';
+  window.location.href = 'Coding-Hut/login.html';
 }
 
 function goToPage5() {
-  window.location.href = '/Coding-Hut/signup.html';
+  window.location.href = 'Coding-Hut/signup.html';
 }
 
 // Login form handling
